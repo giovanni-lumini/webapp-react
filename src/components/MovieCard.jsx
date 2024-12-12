54f997a94c149e41aba35284552449064acd3590
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-12-12 12:23:29 +0100

    style

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function MovieCard({ movie }) {
     return (
         <div className="card">
-            <div className="card-body">
+            <div className="card-body d-flex flex-column justify-content-between">
                 <h4>{movie.title}</h4>
                 <h5>{movie.director}</h5>
                 <p className="mb-3">{movie.abstract}</p>
